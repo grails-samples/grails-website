ebcceb246054e47e00aa9fd8a7efcc4867aac805
Peter Ledbrook <peter@cacoethes.co.uk>
2010-07-16 17:57:38 +0100

    Grails plugin manager no longer logged at debug level.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -97,9 +97,9 @@ log4j = {
            'org.codehaus.groovy.grails.web.pages', //  GSP
 	   'org.codehaus.groovy.grails.web.sitemesh', //  layouts
 	   'org.codehaus.groovy.grails.commons', // core / classloading
+           'org.codehaus.groovy.grails.plugins', // plugins
 	   'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
 	   'org.springframework',
 	   'org.hibernate'
 
-    debug 'org.codehaus.groovy.grails.plugins'
 }
