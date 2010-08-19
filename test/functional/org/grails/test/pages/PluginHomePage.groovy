d3c21c196f4be8e92cd5c1b51671ab41f6dda0e3
Peter Ledbrook <peter@cacoethes.co.uk>
2010-08-19 14:53:40 +0100

    Added search box to Geb plugin home page definition.

--- test/functional/org/grails/test/pages/PluginHomePage.groovy
+++ test/functional/org/grails/test/pages/PluginHomePage.groovy
@@ -8,6 +8,13 @@ class PluginHomePage extends Page {
     static at = {
         assert title == "Grails - Plugins Portal"
         assert $("h1", 0).text() == "Plugins"
+
+        def pluginSearch = $("#pluginSearch")
+        assert pluginSearch.size() == 1
+        assert pluginSearch.find("input", name: "q").size() == 1
+        assert pluginSearch.find("input", value: "Search").size() == 1
+
+        // Helpful Links
         assert $(text: "Helpful Links").size() == 1
         assert $("a", text: "Creating a plugin").@href == "/Creating+Plugins"
         assert $("a", text: "Plugins Fisheye").@href == "http://svn.grails-plugins.codehaus.org/"
