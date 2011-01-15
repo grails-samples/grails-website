b035050f690acf628b6d05a0f0c2fa3a50c2f898
Peter Ledbrook <peter@cacoethes.co.uk>
2011-01-15 09:02:28 +0000

    Added a database migration for the screencast changes.

--- migrations/changelog-1.0.1.groovy
+++ migrations/changelog-1.0.1.groovy
@@ -50,3 +50,33 @@ changeSet(id: "ExtraPluginFields", author: "pledbrook") {
         column name: "group_id", value: "org.grails.plugins"
     }
 }
+
+changeSet(id: "VideoHostForScreencasts", author: "pledbrook") {
+    createTable tableName: "video_host", {
+        column name: "id", type: "bigint", autoIncrement: true, {
+            constraints primaryKey: true, nullable: false
+        }
+        column name: "version", type: "bigint", {
+            constraints nullable: false
+        }
+        column name: "embed_template", type: "text", {
+            constraints nullable: false
+        }
+        column name: "name", type: "varchar(255)", {
+            constraints nullable: false, unique: true
+        }
+    }
+
+    addColumn tableName: "screencast", {
+        column name: "video_host_id", type: "bigint", {
+            constraints nullable: true, foreignKeyName: "FKE72625AB937EA8A1"
+        }
+        column name: "video_id", type: "varchar(255)"
+    }
+
+    addForeignKeyConstraint constraintName: "FKE72625AB937EA8A1",
+                            baseTableName: "screencast",
+                            baseColumnNames: "video_host_id",
+                            referencedTableName: "video_host",
+                            referencedColumnNames: "id"
+}
