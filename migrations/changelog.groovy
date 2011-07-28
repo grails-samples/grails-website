af9ecb952909a6f0c01641a694b5dd7561556089
Peter Ledbrook <peter@cacoethes.co.uk>
2011-07-28 19:57:39 +0100

    changelog-1.1 shouldn't have been in the main changelog. Removed.

--- migrations/changelog.groovy
+++ migrations/changelog.groovy
@@ -8,6 +8,5 @@ databaseChangeLog = {
         include file: "Rhyolight/PluginPortalCommentsMigration.groovy"
         include file: "changelog-1.0.1.groovy"
         include file: "changelog-1.0.5.groovy"
-        include file: "changelog-1.1.groovy"
     }
 }
