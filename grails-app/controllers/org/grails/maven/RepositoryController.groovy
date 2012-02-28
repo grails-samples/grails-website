41c1f0866a737fc025e449f56732eb6fe6f412e3
graemerocher <graeme.rocher@gmail.com>
2012-02-28 11:06:45 +0100

    implemented plugin list generation in a format compatible with existing Grails repository format

--- grails-app/controllers/org/grails/maven/RepositoryController.groovy
+++ grails-app/controllers/org/grails/maven/RepositoryController.groovy
@@ -1,6 +1,7 @@
 package org.grails.maven
 
 import grails.plugin.springcache.annotations.Cacheable
+import org.grails.plugin.*
 
 /**
  * 
@@ -26,8 +27,49 @@ class RepositoryController {
             render status:404
     }
     
+    def pluginService
     
+    /**
+     * Renders the plugin list as XML in a format compatible with all versions of Grails
+     */
+    @Cacheable("pluginMetaList")
     def list() {
-        render "plugin list"
+        def total = Plugin.count()
+        int offset = 0
+        
+        render(contentType:"text/xml") {
+            plugins {
+
+                Plugin.withSession { session ->
+                    
+                    while(total > offset) {
+                        def allPlugins = Plugin.list(fetch:['releases':'eager'], offset:offset, max:10)                    
+                        if(!allPlugins) break
+                        
+                        for(p in allPlugins) {
+                            def latest = p.releases.max { it.releaseDate }
+                            plugin(name:p.name, 'latest-release':latest?.releaseVersion) {
+                                p.releases?.each { r ->
+                                    release(version:r.releaseVersion) {
+                                        title p.title
+                                        author p.author
+                                        authorEmail p.authorEmail
+                                        description p.summary
+                                        file r.downloadUrl
+                                    }
+                                }
+
+                            }
+                        }
+                        offset += 10
+                        session.clear()                        
+                    }
+                    
+
+                }
+                
+            }
+        }
+        
     }
 }
--- test/unit/org/grails/maven/RepositoryControllerSpec.groovy
+++ test/unit/org/grails/maven/RepositoryControllerSpec.groovy
@@ -1,8 +1,11 @@
 package org.grails.maven
 
 import grails.test.mixin.*
+import org.grails.plugin.*
+import org.joda.time.DateTime
 
 @TestFor(RepositoryController)
+@Mock([Plugin, PluginRelease])
 class RepositoryControllerSpec extends spock.lang.Specification{
 
     // http://localhost:8080/maven/grails-acegi/tags/RELEASE_0_1/grails-acegi-0.1.zip
@@ -19,5 +22,41 @@ class RepositoryControllerSpec extends spock.lang.Specification{
             
         then:"The produced URL is correct"    
             response.redirectUrl == 'http://repo.grails.org/grails/plugins/org/grails/plugins/cloud-foundry/1.0/cloud-foundry-1.0.zip'
+    }
+    
+    void "Test that the plugin list generates XML in the right format"() {
+        given:"A plugin with a release"
+            def p = new Plugin( name: "tomcat",
+                                title: "Tomcat",
+                                currentRelease: "1.3.4",
+                                author: "SpringSource",
+                                authorEmail:'foo@bar.com',
+                                downloadUrl:"http://foo.com/tomcat-1.0.zip",
+                                documentationUrl:"http://grails.org/plugin/tomcat",
+                                lastReleased: new DateTime(2010, 8, 11, 22, 30))
+            p.releases = [ new PluginRelease(plugin:p, releaseVersion:"1.0.0", releaseDate:new DateTime(2010, 8, 11, 22, 30), downloadUrl:"http://foo.com/tomcat-1.0.zip")]
+            p.save(flush:true)
+            assert !p.hasErrors()
+            
+        when:"The domain model is queried"
+            def allPlugins = Plugin.list()
+            
+        then:"It is in the correct state"
+            allPlugins.size() == 1
+            allPlugins[0].releases.size() == 1
+            
+        when:"The plugin list is rendered"
+            controller.list()
+            println response.text
+        then:"The generated xml is correct"
+            response.xml.plugin.size() == 1
+            response.xml.plugin[0].@name == 'tomcat'
+            response.xml.plugin[0].'@latest-release' == '1.0.0'            
+            response.xml.plugin[0].release.size() == 1
+            response.xml.plugin[0].release[0].@version == '1.0.0'
+            response.xml.plugin[0].release[0].author.text() == 'SpringSource'
+            response.xml.plugin[0].release[0].authorEmail.text() == 'foo@bar.com'
+            response.xml.plugin[0].release[0].file.text() == "http://foo.com/tomcat-1.0.zip"
+
     }
 }
