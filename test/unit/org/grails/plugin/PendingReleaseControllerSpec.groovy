e6cb34a2812cd597d7c585d4b3ddf95daf606264
graemerocher <graeme.rocher@gmail.com>
2012-03-02 17:49:45 +0100

    test for PendingReleaseController

--- test/unit/org/grails/plugin/PendingReleaseControllerSpec.groovy
+++ test/unit/org/grails/plugin/PendingReleaseControllerSpec.groovy
@@ -5,22 +5,27 @@ import static org.junit.Assert.*
 import grails.test.mixin.*
 import grails.test.mixin.support.*
 import org.junit.*
-
+import org.grails.maven.*
 /**
  * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
  */
-@TestMixin(GrailsUnitTestMixin)
-class PendingReleaseControllerSpec {
+@TestFor(PendingReleaseController)
+@Mock(PendingRelease)
+class PendingReleaseControllerSpec extends spock.lang.Specification{
 
-    void setUp() {
-        // Setup logic here
-    }
 
-    void tearDown() {
-        // Tear down logic here
-    }
+    void "Test deploy action"() {
+        given:"A pending release"
+            def pr = new PendingRelease(pluginName:"tomcat", pluginVersion:"1.0.0", zip:"dummy".bytes, pom:"dummy".bytes, xml:"dummy".bytes)
 
-    void testSomething() {
-        fail "Implement me"
+            assert pr.save( flush:true)
+
+            PluginDeployService pluginDeployService = Mock()
+            controller.pluginDeployService = pluginDeployService
+        when:"The deploy action is called"
+            controller.deploy(pr.id)
+
+        then:"The deploy service is invoked"
+            1 * pluginDeployService.deployRelease(pr)
     }
 }
