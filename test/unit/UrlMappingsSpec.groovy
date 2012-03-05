91a8f17f2669ff5420698dc84f14fdd01f89e52b
Peter Ledbrook <peter@cacoethes.co.uk>
2012-03-05 11:01:27 +0000

    Fix URL mappings unit test.
    Apparently you have to @Mock the controllers you want to test in URL mappings. Done that, and now the tests are passing.

--- test/unit/UrlMappingsSpec.groovy
+++ test/unit/UrlMappingsSpec.groovy
@@ -1,11 +1,19 @@
+import org.grails.maven.RepositoryController
 import spock.lang.*
 
 @TestFor(UrlMappings)
+@Mock(RepositoryController)
 class UrlMappingsSpec extends Specification {
+    def "Master plugin list"() {
+        expect:
+        assertForwardUrlMapping "/plugins/.plugin-meta/plugins-list.xml",
+                controller: 'repository', action: 'list'
+    }
+
     def "Grails plugin repository URLs"() {
         expect:
         assertForwardUrlMapping "/plugins/grails-$plugin/tags/RELEASE_${version.replaceAll('\\.', '_')}/grails-$plugin-$version.${type}",
-                controller: 'repository', action: 'list'
+                controller: 'repository', action: 'artifact'
                 [plugin: plugin, version: version, fullName: "$plugin-$version", type: type]
 
         where:
