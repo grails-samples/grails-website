013af2c5f70b68441de4a1f2103e8ff88f1d57e9
Peter Ledbrook <peter@cacoethes.co.uk>
2012-05-28 11:36:37 +0100

    Ensure migration scripts are always included in WAR.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -225,11 +225,9 @@ grails.gorm.default.mapping = {
 
 
 // Enable these in a site-config.groovy file.
-/*
 grails.plugin.databasemigration.changelogLocation = "migrations"
-grails.plugin.databasemigration.updateOnStart = true
+grails.plugin.databasemigration.updateOnStart = false
 grails.plugin.databasemigration.updateOnStartFileNames = ["changelog.groovy"]
-*/
 
 // log4j configuration
 log4j = {
