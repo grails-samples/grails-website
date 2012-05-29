b1cf59879653220bfc2a1fffd2d71f9cc10b967a
Peter Ledbrook <peter@cacoethes.co.uk>
2012-05-29 13:40:58 +0100

    Make event bus listeners void methods.

--- grails-app/services/org/grails/search/IndexingService.groovy
+++ grails-app/services/org/grails/search/IndexingService.groovy
@@ -7,13 +7,13 @@ import org.grails.plugin.Plugin
 class IndexingService {
 
     @Listener
-    def pluginUpdated(Map message) {
+    void pluginUpdated(Map message) {
         log.info "Reindexing plugin with ID ${message.id}"
         Plugin.reindex(message.id)
     }
 
     @Listener
-    def wikiPageUpdated(Map message) {
+    void wikiPageUpdated(Map message) {
         log.info "Reindexing wiki page with ID ${message.id}"
         Content.reindex(message.id)
     }
