f9a7bbdb3e0bcfa6d73a658681662df3b7ae61a9
Peter Ledbrook <peter@cacoethes.co.uk>
2012-08-01 15:20:29 +0100

    Fix downloads fixtures file.
    It was referencing old constant fields on DownloadFile, but these were
    replaced with an enum.

--- fixtures/downloads.groovy
+++ fixtures/downloads.groovy
@@ -1,4 +1,4 @@
-import org.grails.downloads.*
+import org.grails.downloads.DownloadFile
 import org.joda.time.DateTime
 
 fixture {
@@ -9,10 +9,10 @@ fixture {
             betaRelease: true,
             releaseDate: new DateTime(2011, 4, 30, 0, 0),
             files: [
-                [ title: "Binary Zip", fileType: DownloadFile.TYPE_BINARY, mirrors: [
+                [ title: "Binary Zip", fileType: DownloadFile.FileType.BINARY, mirrors: [
                     new Mirror(name: "grails.org", urlString: "http://grails.org/dist/grails-1.4.0.M1.zip"),
                     new Mirror(name: "Amazon S3", urlString: "http://s3.amazon.com/somebucket/grails-1.4.0.M1.zip") ]],
-                [ title: "Documentation", fileType: DownloadFile.TYPE_DOCUMENTATION, mirrors: [
+                [ title: "Documentation", fileType: DownloadFile.FileType.DOCUMENTATION, mirrors: [
                     new Mirror(name: "grails.org", urlString: "http://grails.org/dist/grails-docs-1.4.0.M1.zip") ]]])
 
     download138(Download,
@@ -22,10 +22,10 @@ fixture {
             betaRelease: true,
             releaseDate: new DateTime(2011, 2, 13, 0, 0),
             files: [
-                [ title: "Binary Zip", fileType: DownloadFile.TYPE_BINARY, mirrors: [
+                [ title: "Binary Zip", fileType: DownloadFile.FileType.BINARY, mirrors: [
                     new Mirror(name: "grails.org", urlString: "http://grails.org/dist/grails-1.3.8.BUILD-SNAPSHOT.zip"),
                     new Mirror(name: "Amazon S3", urlString: "http://s3.amazon.com/somebucket/grails-1.3.8.BUILD-SNAPSHOT.zip") ]],
-                [ title: "Documentation", fileType: DownloadFile.TYPE_DOCUMENTATION, mirrors: [
+                [ title: "Documentation", fileType: DownloadFile.FileType.DOCUMENTATION, mirrors: [
                     new Mirror(name: "grails.org", urlString: "http://grails.org/dist/grails-docs-1.3.8.BUILD-SNAPSHOT.zip") ]]])
 
     download137(Download,
@@ -34,10 +34,10 @@ fixture {
             softwareVersion: "1.3.7",
             releaseDate: new DateTime(2011, 1, 13, 0, 0),
             files: [
-                [ title: "Binary Zip", fileType: DownloadFile.TYPE_BINARY, mirrors: [
+                [ title: "Binary Zip", fileType: DownloadFile.FileType.BINARY, mirrors: [
                     new Mirror(name: "grails.org", urlString: "http://grails.org/dist/grails-1.3.7.zip"),
                     new Mirror(name: "Amazon S3", urlString: "http://s3.amazon.com/somebucket/grails-1.3.7.zip") ]],
-                [ title: "Documentation", fileType: DownloadFile.TYPE_DOCUMENTATION, mirrors: [
+                [ title: "Documentation", fileType: DownloadFile.FileType.DOCUMENTATION, mirrors: [
                     new Mirror(name: "grails.org", urlString: "http://grails.org/dist/grails-docs-1.3.7.zip") ]]])
 
     download125(Download,
@@ -46,9 +46,9 @@ fixture {
             softwareVersion: "1.2.5",
             releaseDate: new DateTime(2010, 10, 4, 0, 0),
             files: [
-                [ title: "Binary Zip", fileType: DownloadFile.TYPE_BINARY, mirrors: [
+                [ title: "Binary Zip", fileType: DownloadFile.FileType.BINARY, mirrors: [
                     new Mirror(name: "Codehaus", urlString: "http://dist.codehaus.org/grails/grails-1.2.5.zip") ]],
-                [ title: "Documentation", fileType: DownloadFile.TYPE_DOCUMENTATION, mirrors: [
+                [ title: "Documentation", fileType: DownloadFile.FileType.DOCUMENTATION, mirrors: [
                     new Mirror(name: "Codehaus", urlString: "http://dist.codehaus.org/grails/grails-docs-1.2.5.zip"),
                     new Mirror(name: "Amazon S3", urlString: "http://s3.amazon.com/somebucket/grails-docs-1.2.5.zip") ]]])
 
