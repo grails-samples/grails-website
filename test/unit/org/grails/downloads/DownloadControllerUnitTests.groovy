245a55d6d1eeaefb4a76ff0db1f0240c19b92353
Graeme Rocher <graeme.rocher@gmail.com>
2012-11-07 14:27:52 +0100

    fix download controller unit tests

--- test/unit/org/grails/downloads/DownloadControllerUnitTests.groovy
+++ test/unit/org/grails/downloads/DownloadControllerUnitTests.groovy
@@ -12,23 +12,20 @@ class DownloadControllerUnitTests {
 
        assert new VersionOrder(baseVersion:"2.0",orderIndex:1).save()
        assert new VersionOrder(baseVersion:"1.3", orderIndex:2).save()
-       new Download(softwareName: "Grails", softwareVersion: "2.0.2").save() 
-       new Download(softwareName: "Grails", softwareVersion: "1.3.4").save(flush:true) 
+       new Download(softwareName: "Grails", softwareVersion: "2.0.2")
+            .addToFiles(fileType:DownloadFile.FileType.BINARY)
+            .save() 
+       new Download(softwareName: "Grails", softwareVersion: "1.3.4")
+            .addToFiles(fileType:DownloadFile.FileType.BINARY)
+            .save(flush:true) 
         
-        controller.metaClass.fetchMode = { String name, type ->
-            // ignore
-        }
-        controller.grailsApplication = [config: [download: [versions: ["2.0", "1.3"]]]]
-        controller.downloadCache = new Expando()
-        controller.downloadCache.get = { String name -> delegate."${name}" }
-        controller.downloadCache.put = { element -> delegate."${element.key}" = element.value }
-
-        this.controller.latest()
+       controller.downloadService = new DownloadService()
+        def model = this.controller.index()
         
-        def stableDownloads = model["stableDownloads"]
+        def stableDownloads = model["majorVersions"]
         assertEquals 2, stableDownloads.size()
-        assertEquals([ "2.0", "1.3" ], stableDownloads.keySet() as List)
-        assert "2.0.2" == stableDownloads["2.0"][0].softwareVersion
-        assert "1.3.4" == stableDownloads["1.3"][0].softwareVersion
+        assertEquals([ "2.0", "1.3" ], stableDownloads)
+        assert "2.0.2" == model.groupedDownloads["2.0"][0].download.softwareVersion
+        assert "1.3.4" == model.groupedDownloads["1.3"][0].download.softwareVersion
     }
 }
