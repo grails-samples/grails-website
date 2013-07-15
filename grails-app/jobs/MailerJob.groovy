e0f861e02ade523c60b4fb20d567b14788339204
Graeme Rocher <graeme.rocher@gmail.com>
2013-07-15 14:19:42 +0200

    fix NPE

--- grails-app/jobs/MailerJob.groovy
+++ grails-app/jobs/MailerJob.groovy
@@ -41,51 +41,55 @@ class MailerJob {
 
                     def pageClass = classLoader.loadClass(pageUpdate.className)
                     def content = pageClass.findByTitle(pageUpdate.title)
-                        
-                    def text = new StringBuilder()
-                    def titleUrlEscaped = pageUpdate.title.encodeAsURL()
-                    def url = "http://grails.org/${titleUrlEscaped}"
-                    def myTitle = pageUpdate.title
-                    // make some alterations to the email if this wiki is a part of a plugin
-                    if (content.instanceOf(PluginTab)) {
-                        def plugin = content.plugin
-                        url = "http://grails.org/plugin/${plugin.name}"
-                        def titleParts = content.title.split('-')
-                        
-                        // The plugin tab type is encoded in the page title in different
-                        // ways depending on when the page was created. The old style is
-                        // '$wikiType-nnn, whereas the new style is 'plugin-$pluginName-$wikiType'.
-                        def wikiType
-                        if (titleParts[1] ==~ /\d+/) wikiType = titleParts[0]
-                        else wikiType = titleParts[-1]
-                        
-                        myTitle = "${plugin.title} (${wikiType[0].toUpperCase() + wikiType[1..-1]} tab)"
+
+                    if(content != null) {
+                        def text = new StringBuilder()
+                        def titleUrlEscaped = pageUpdate.title.encodeAsURL()
+                        def url = "http://grails.org/${titleUrlEscaped}"
+                        def myTitle = pageUpdate.title
+                        // make some alterations to the email if this wiki is a part of a plugin
+                        if (content.instanceOf(PluginTab)) {
+                            def plugin = content.plugin
+                            url = "http://grails.org/plugin/${plugin.name}"
+                            def titleParts = content.title.split('-')
+                            
+                            // The plugin tab type is encoded in the page title in different
+                            // ways depending on when the page was created. The old style is
+                            // '$wikiType-nnn, whereas the new style is 'plugin-$pluginName-$wikiType'.
+                            def wikiType
+                            if (titleParts[1] ==~ /\d+/) wikiType = titleParts[0]
+                            else wikiType = titleParts[-1]
+                            
+                            myTitle = "${plugin.title} (${wikiType[0].toUpperCase() + wikiType[1..-1]} tab)"
+                        }
+
+                        def pageVersions = Version.findAllByCurrent(content, [sort:'number',order:'desc', max:1, cache:true])
+
+                        if (pageVersions) {
+                            def version = pageVersions[-1] //last version
+                            text << "<div>To unsubscribe from receiving these emails go to <a href=\"http://grails.org/profile\">http://grails.org/profile</a>, login and uncheck the 'Receive E-mail Updates for Content Changes?' box. </div><br><br>"
+                            text << '<div style="color:black;"><br><br>'
+                            text << "Page <a href=\"${url}\">${myTitle}<a/> <br><br>"
+                            text << "</div>"
+                            text << '<div style="color:black;"><br><br>'
+                            text << "Edited by <b>${version.author?.login}</b>. <a href=\"http://grails.org/previous/${titleUrlEscaped}/${version.number}\">View change</a> <br><br>"
+                            text << "</div>"
+    //                        text << engine.render(content.body, context)
+                            text = text.toString()
+
+                            for (email in emails) {
+                                mailService?.sendMail {
+                                    title "Grails > ${myTitle}"
+                                    from "wiki@grails.org"
+                                    replyTo "wiki@grails.org"
+                                    to email
+                                    html text
+                                }
+                            }                        
+                        }
+                            
                     }
-
-                    def pageVersions = Version.findAllByCurrent(content, [sort:'number',order:'desc', max:1, cache:true])
-
-                    if (pageVersions) {
-                        def version = pageVersions[-1] //last version
-                        text << "<div>To unsubscribe from receiving these emails go to <a href=\"http://grails.org/profile\">http://grails.org/profile</a>, login and uncheck the 'Receive E-mail Updates for Content Changes?' box. </div><br><br>"
-                        text << '<div style="color:black;"><br><br>'
-                        text << "Page <a href=\"${url}\">${myTitle}<a/> <br><br>"
-                        text << "</div>"
-                        text << '<div style="color:black;"><br><br>'
-                        text << "Edited by <b>${version.author?.login}</b>. <a href=\"http://grails.org/previous/${titleUrlEscaped}/${version.number}\">View change</a> <br><br>"
-                        text << "</div>"
-//                        text << engine.render(content.body, context)
-                        text = text.toString()
-
-                        for (email in emails) {
-                            mailService?.sendMail {
-                                title "Grails > ${myTitle}"
-                                from "wiki@grails.org"
-                                replyTo "wiki@grails.org"
-                                to email
-                                html text
-                            }
-                        }                        
-                    }
+                        
                     pageUpdate = wikiPageUpdates.poll()
                     Content.withSession { session -> session.clear() }
                 }
