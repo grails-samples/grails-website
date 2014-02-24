aa75c799f2b5b9da0f962e52d04877cfd1eaf173
Lari Hotari <lhotari@gopivotal.com>
2014-02-24 11:08:21 +0200

    WEBSITE-275 add proper defaults for production dataSource properties

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -33,11 +33,29 @@ environments {
     }
     production {
         dataSource {
+            pooled = true
             dbCreate = "update"
             url = "jdbc:h2:prodDb"
             driverClassName = "org.h2.Driver"
             username = "sa"
             password = ""
+            jmxEnabled = true
+            initialSize = 5
+            maxActive = 50
+            minIdle = 5
+            maxIdle = 25
+            maxWait = 10000
+            maxAge = 10 * 60000
+            timeBetweenEvictionRunsMillis = 5000
+            minEvictableIdleTimeMillis = 60000
+            validationQuery = "SELECT 1"
+            validationQueryTimeout = 3
+            validationInterval = 15000
+            testOnBorrow = true
+            testWhileIdle = true
+            testOnReturn = false
+            jdbcInterceptors = "ConnectionState;StatementCache(max=200)"
+            defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED            
         }
     }
 }
