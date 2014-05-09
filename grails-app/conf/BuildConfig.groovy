e9d9e98cbb832d60a6461bd15258aabbe875ec0b
Lari Hotari <lhotari@gopivotal.com>
2014-05-09 17:15:56 +0300

    set work dir

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -4,6 +4,8 @@ grails.project.work.dir = "target/$grailsVersion"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.plugins.dir = "plugins"
 
+grails.project.work.dir = "target/work"
+
 grails.project.dependency.resolver="maven"
 grails.project.fork = [
    // test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true], // configure settings for the test-app JVM
