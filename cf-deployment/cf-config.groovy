f3d1519090659ee7db579891992e645c77fd8c29
Lari Hotari <lhotari@gopivotal.com>
2014-05-09 23:11:37 +0300

    add example cloudfoundry sendgrid config

---
+++ cf-deployment/cf-config.groovy
@@ -0,0 +1,22 @@
+import org.springframework.cloud.CloudFactory
+
+def cloud
+
+try {
+  cloud = new CloudFactory().cloud
+} catch(e) {}
+
+grails.serverURL = "http://grailsorg-dev.cfapps.io/"
+
+// configure sendgrid SMTP sender
+grails {
+   mail {
+     def smtpServiceInfo = cloud.getServiceInfo('sendgrid')
+     host = "smtp.sendgrid.net"
+     port = 587
+     username = smtpServiceInfo.userName
+     password = smtpServiceInfo.password
+     props = ["mail.smtp.starttls.enable":"true", 
+              "mail.smtp.port":"587"]
+   }
+}
