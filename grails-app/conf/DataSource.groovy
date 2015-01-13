0e6ac490c76d129621a0716e32b211ece8afe267
Lari Hotari <lhotari@pivotal.io>
2015-01-13 15:30:19 -0500

    update DataSource.groovy

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,11 +1,10 @@
-//dataSource {
-//    pooled = true
-//    driverClassName = "com.mysql.jdbc.Driver"
-//    url = "jdbc:mysql://localhost/grails"
-//    username = "root"
-//    password = ""
-//    dialect = org.grails.db.Utf8InnodbDialect
-//}
+dataSource {
+    pooled = true
+    jmxExport = true
+    driverClassName = "org.h2.Driver"
+    username = "sa"
+    password = ""
+}
 hibernate {
     cache.use_second_level_cache=false
     cache.use_query_cache=false
@@ -15,47 +14,40 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "update" // one of 'create', 'create-drop','update'
-            url = "jdbc:h2:mem:devDb"
-            driverClassName = "org.h2.Driver"
-            username = "sa"
-            password = ""
+            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
     test {
         dataSource {
-            dbCreate = "create-drop"
-            url = "jdbc:h2:mem:testDb"
-            driverClassName = "org.h2.Driver"
-            username = "sa"
-            password = ""
+            dbCreate = "update"
+            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
     production {
         dataSource {
-            pooled = true
             dbCreate = "update"
-            url = "jdbc:h2:prodDb"
-            driverClassName = "org.h2.Driver"
-            username = "sa"
-            password = ""
-            jmxEnabled = true
-            initialSize = 5
-            maxActive = 50
-            minIdle = 5
-            maxIdle = 25
-            maxWait = 10000
-            maxAge = 10 * 60000
-            timeBetweenEvictionRunsMillis = 5000
-            minEvictableIdleTimeMillis = 60000
-            validationQuery = "SELECT 1"
-            validationQueryTimeout = 3
-            validationInterval = 15000
-            testOnBorrow = true
-            testWhileIdle = true
-            testOnReturn = false
-            jdbcInterceptors = "ConnectionState;StatementCache(max=200)"
-            defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED            
+            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            properties {
+               // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
+               jmxEnabled = true
+               initialSize = 5
+               maxActive = 50
+               minIdle = 5
+               maxIdle = 25
+               maxWait = 10000
+               maxAge = 10 * 60000
+               timeBetweenEvictionRunsMillis = 5000
+               minEvictableIdleTimeMillis = 60000
+               validationQuery = "SELECT 1"
+               validationQueryTimeout = 3
+               validationInterval = 15000
+               testOnBorrow = true
+               testWhileIdle = true
+               testOnReturn = false
+               jdbcInterceptors = "ConnectionState"
+               defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+            }
         }
     }
 }
