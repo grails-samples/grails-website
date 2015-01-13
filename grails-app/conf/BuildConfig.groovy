a53bb36790ecbc8da6afeb7240ce3364dace4ded
Lari Hotari <lhotari@pivotal.io>
2015-01-13 15:31:11 -0500

    update BuildConfig.groovy

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,12 +1,15 @@
 grails.plugin.location.like = "${basedir}/sections/like"
 
-grails.project.work.dir = "target/$grailsVersion"
+grails.servlet.version = "3.0" // Change depending on target container compliance (2.5 or 3.0)
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = "target/work"
+grails.project.target.level = 1.6
+grails.project.source.level = 1.6
+//grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.plugins.dir = "target/plugins"
 
-grails.project.work.dir = "target/work"
-
-grails.project.dependency.resolver="maven"
 grails.project.fork = [
    // test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true], // configure settings for the test-app JVM
    run: [maxMemory: 1024, minMemory: 256, debug: false, maxPerm: 256], // configure settings for the run-app JVM
@@ -19,19 +22,22 @@ if (System.getProperty("grails.debug")) {
    println "Using debug for run-war"
 }
 
+grails.project.dependency.resolver="maven"
 grails.project.dependency.resolution = {
     inherits "global", {
         excludes "xml-apis", "commons-digester", "ehcache", 'cglib'
     }
 
-    log "warn"
+    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    checksums true // Whether to verify checksums on resolve
+    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
     repositories {
         inherit false
         grailsPlugins()
         grailsHome()
-        grailsCentral()
         mavenLocal()
+        grailsCentral()
         mavenRepo "https://repo.grails.org/grails/core"
         mavenRepo "https://oss.sonatype.org/content/repositories/releases/"
         mavenRepo "http://repo.spring.io/milestone"
