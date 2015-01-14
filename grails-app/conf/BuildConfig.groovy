7f5427f0769868ab0bd77d194205b451b3263f36
Lari Hotari <lhotari@pivotal.io>
2015-01-14 11:16:00 -0500

    CloudFoundry java-build-pack detects JVM version from class files

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -5,8 +5,8 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.work.dir = "target/work"
-grails.project.target.level = 1.6
-grails.project.source.level = 1.6
+grails.project.target.level = 1.7
+grails.project.source.level = 1.7
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.plugins.dir = "target/plugins"
 
