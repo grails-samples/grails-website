44f65fd2c4c38a0700911028793ecb69b1ea2625
Lari Hotari <lhotari@pivotal.io>
2015-01-16 16:39:57 -0500

    update CF url

--- cf-deployment/add_config.sh
+++ cf-deployment/add_config.sh
@@ -4,6 +4,6 @@ if [ -d WEB-INF ]; then
    zip -r ../target/site-*.war WEB-INF
 else
    mkdir -p WEB-INF/classes
-   echo 'grails.serverURL="http://plugins-grails-org-dev.cfapps.io/"' > WEB-INF/classes/site-config.groovy
+   echo 'grails.serverURL="http://staging.grails.org/"' > WEB-INF/classes/site-config.groovy
    echo 'edit WEB-INF/classes/site-config.groovy and re-run this script'
 fi
