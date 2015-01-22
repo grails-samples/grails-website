65432654a16569cc89fe837b6ea963678a420dc6
Lari Hotari <lhotari@pivotal.io>
2015-01-22 17:59:10 -0500

    add CF mysql database config - based on http://docs.cloudfoundry.org/buildpacks/java/gsg-grails.html

--- cf-deployment/cf-config.groovy
+++ cf-deployment/cf-config.groovy
@@ -1,3 +1,4 @@
+/*
 import org.springframework.cloud.CloudFactory
 
 def cloud
@@ -6,7 +7,6 @@ try {
   cloud = new CloudFactory().cloud
 } catch(e) {}
 
-grails.serverURL = "http://plugins-grails-org-dev.cfapps.io/"
 
 // configure sendgrid SMTP sender
 grails {
@@ -19,4 +19,38 @@ grails {
      props = ["mail.smtp.starttls.enable":"true", 
               "mail.smtp.port":"587"]
    }
+}
+*/
+grails.serverURL = "http://staging.grails.org/"
+
+dataSource {
+    dbCreate = ""
+    pooled = true
+    jmxExport = true
+    driverClassName = "com.mysql.jdbc.Driver"
+    dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+    def dbUri = new URI(System.env.DATABASE_URL ?: "mysql://foo:bar@localhost")
+    username = dbUri.userInfo ? dbUri.userInfo.split(":")[0] : ""
+    password = dbUri.userInfo ? dbUri.userInfo.split(":")[1] : ""
+    url = "jdbc:mysql://" + dbUri.host + dbUri.path
+    properties {
+       // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
+       jmxEnabled = true
+       initialSize = 3
+       maxActive = 14
+       minIdle = 3
+       maxIdle = 10
+       maxWait = 10000
+       maxAge = 10 * 60000
+       timeBetweenEvictionRunsMillis = 5000
+       minEvictableIdleTimeMillis = 60000
+       validationQuery = "SELECT 1"
+       validationQueryTimeout = 3
+       validationInterval = 15000
+       testOnBorrow = true
+       testWhileIdle = true
+       testOnReturn = false
+       jdbcInterceptors = "ConnectionState"
+       defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+    }
 }
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -141,6 +141,8 @@ grails.project.dependency.resolution = {
         test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
 
         compile 'org.ajoberstar:grgit:0.4.0'
+
+        runtime 'mysql:mysql-connector-java:5.1.34'
     }
 
 }
