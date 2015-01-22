17f0b0f1104fde65a7d556d96703e88a9f8cad9e
Lari Hotari <lhotari@pivotal.io>
2015-01-22 16:14:04 -0500

    mysql scripts for creating test database and user for it

---
+++ mysql/create_db.sql
@@ -0,0 +1,2 @@
+drop database if exists grails_org;
+create database grails_org DEFAULT CHARACTER SET = utf8 DEFAULT COLLATE utf8_swedish_ci;
---
+++ mysql/create_db_user.sql
@@ -0,0 +1,4 @@
+GRANT USAGE ON grails_org.* TO 'grails_org'@'%' IDENTIFIED BY 'secretword' WITH GRANT OPTION;
+GRANT ALL ON grails_org.* TO 'grails_org'@'%';
+GRANT USAGE ON grails_org.* TO 'grails_org'@'localhost' IDENTIFIED BY 'secretword' WITH GRANT OPTION;
+GRANT ALL ON grails_org.* TO 'grails_org'@'localhost';
