a13953277098183adf24330fa68bb211f2614bb6
Lari Hotari <lhotari@pivotal.io>
2015-01-28 15:32:57 -0500

    upgrade to CodeMirror version 4.12.0

--- grails-app/assets/javascripts/codeMirror.js
+++ grails-app/assets/javascripts/codeMirror.js
@@ -1,8 +1,8 @@
 //= require libs/codemirror/codemirror.js
-//= require libs/codemirror/util/closetag.js
-//= require libs/codemirror/util/overlay.js
-//= require libs/codemirror/util/runmode.js
-//= require libs/codemirror/mode/xml/xml.js
-//= require libs/codemirror/mode/javascript/javascript.js
+//= require libs/codemirror/addon/edit/closetag.js
+//= require libs/codemirror/addon/mode/overlay.js
+//= require libs/codemirror/addon/runmode/runmode.js
 //= require libs/codemirror/mode/css/css.js
 //= require libs/codemirror/mode/htmlmixed/htmlmixed.js
+//= require libs/codemirror/mode/javascript/javascript.js
+//= require libs/codemirror/mode/xml/xml.js
