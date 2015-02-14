b75d596799b0aad2878be9d28cb2de5a8293402b
Lari Hotari <lhotari@pivotal.io>
2015-02-13 22:00:19 -0500

    add config for CF development space

--- travis-encrypt-files.sh
+++ travis-encrypt-files.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 cat cf-deploy.gradle.properties | ./travis/crypt_stream CF_FILES_CRYPT_KEY > cf-deploy.gradle.properties.gpg
-tar zcvf - cf-deployment-production | ./travis/crypt_stream CF_FILES_CRYPT_KEY > cf-deploy-war.tar.gz.gpg
+tar zcvf - cf-deployment-production cf-deployment-development | ./travis/crypt_stream CF_FILES_CRYPT_KEY > cf-deploy-war.tar.gz.gpg
