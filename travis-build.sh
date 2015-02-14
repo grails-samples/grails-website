155c54527814c03a13af5e0ccf38775d9d27f489
Lari Hotari <lhotari@pivotal.io>
2015-02-14 00:00:10 -0500

    always deploy to development first and to production also if tagged with prod_*

--- travis-build.sh
+++ travis-build.sh
@@ -1,21 +1,13 @@
 #!/bin/bash -xe
-./grailsw refresh-dependencies --non-interactive
-./grailsw compile --non-interactive
-./grailsw test-app :unit --non-interactive
-if [[ $TRAVIS_BRANCH == master && $TRAVIS_REPO_SLUG == "grails-samples/grails-website"
-    && $TRAVIS_PULL_REQUEST == 'false' ]]; then
-  DEPLOY_ARGS=""
-  # push to production when tagged with tag starting with prod_
-  if [[ $TRAVIS_TAG == prod_* ]]; then
-    DEPLOY_ARGS="-Pprod"
-    export CF_SPACE=production
-  else
-    export CF_SPACE=development
-  fi
-  ./grailsw prod war --non-interactive
-  ./travis-decrypt-files.sh
+deploy_to_cf {
+  CF_SPACE=$1
   if [ -d "cf-deployment-$CF_SPACE" ]; then
     (cd "cf-deployment-$CF_SPACE" && zip -r ../target/grails-website.war * )
+  fi
+  if [[ $CF_SPACE == production ]]; then
+    DEPLOY_ARGS="-Pprod"
+  else
+    DEPLOY_ARGS=""
   fi
   gradle_cf_deploy="gradle -b cf-deploy.gradle $DEPLOY_ARGS"
   $gradle_cf_deploy cfDeploy
@@ -26,9 +18,30 @@ if [[ $TRAVIS_BRANCH == master && $TRAVIS_REPO_SLUG == "grails-samples/grails-we
     # swap blue/green after successful deployment and undeploy other
     $gradle_cf_deploy cfSwapDeployed cfUndeploy
   else
+    set +x
     echo "Using blue-green deployment. Deployed to either one. NOT ACTIVATED BY DEFAULT to the default route!"
     echo -e "You should manually swap the active route with this command in your local build environment:\n$gradle_cf_deploy cfSwapDeployed cfUndeploy"
     echo "You must specify cfUsername and cfPassword in the cf-deploy.gradle.properties file in that case."
     echo "If you are unable to do this, push another commit with [cf-deploy] in the commit message and tag it with the production tag prod_"
+  fi
+}
+
+./grailsw refresh-dependencies --non-interactive
+./grailsw compile --non-interactive
+./grailsw test-app :unit --non-interactive
+if [[ ( $TRAVIS_BRANCH == master || $TRAVIS_TAG == prod_* ) && $TRAVIS_REPO_SLUG == "grails-samples/grails-website"
+    && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+  DEPLOY_ARGS=""
+  ./grailsw war --non-interactive
+  # make backup of war file
+  cp target/grails-website.war{,.orig}
+  ./travis-decrypt-files.sh
+  # always deploy to development first
+  deploy_to_cf development
+  # restore original war file after deployment
+  cp target/grails-website.war{.orig,}
+  # push to production if commit is tagged with tag starting with prod_
+  if [[ $TRAVIS_TAG == prod_* ]]; then
+    deploy_to_cf production
   fi
 fi
