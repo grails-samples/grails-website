1f0e042664693931aa2a599a6ed02d0df8008b83
Lari Hotari <lhotari@pivotal.io>
2015-02-14 00:13:14 -0500

    remove unneeded cf deployment files, replaced by cf-deploy.gradle

--- cf-deployment/add_config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd "$( dirname "${BASH_SOURCE[0]}")"
-if [ -d WEB-INF ]; then
-   zip -r ../target/grails-website.war WEB-INF
-else
-   mkdir -p WEB-INF/classes
-   echo 'grails.serverURL="http://staging.grails.org/"' > WEB-INF/classes/site-config.groovy
-   echo 'edit WEB-INF/classes/site-config.groovy and re-run this script'
-fi
