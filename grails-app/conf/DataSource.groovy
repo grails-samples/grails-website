97e80d7350713d9b6f946ba1870951e7083fe3b8
Lari Hotari <lhotari@pivotal.io>
2015-02-19 01:00:28 -0500

    configure ehcache for hibernate and enable hibernate caching

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -7,12 +7,15 @@ dataSource {
 }
 
 hibernate {
-    cache.use_second_level_cache = false
-    cache.use_query_cache = false
+    cache.use_second_level_cache = true
+    cache.use_query_cache = true
     cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
     //cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
     singleSession = true // configure OSIV singleSession mode
     //flush.mode = 'manual' // OSIV session flush mode outside of transactional context
+    
+    cache.use_minimal_puts=false
+    cache.provider_configuration_file_resource_path='/ehcache-hibernate.xml'
 }
 
 // environment specific settings
